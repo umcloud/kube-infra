0a8ab96035e6914b3cdeb67f11cc6a59a71bc064
JuanJo Ciarlante <juanjosec@gmail.com>
2018-06-15 19:30:48 +0000

    [jjo] 51-ceph-sync-manifests.sh: mkdir -p  manifests/ceph

--- 51-ceph-sync-manifests.sh
+++ 51-ceph-sync-manifests.sh
@@ -1,4 +1,5 @@
 files="clusterrole.yaml clusterrolebinding.yaml deployment.yaml role.yaml rolebinding.yaml serviceaccount.yaml storage-class.yaml"
+mkdir -p manifests/ceph
 cd manifests/ceph || exit 1
 rm -f *.yaml
 printf "%s\n" $files | xargs -I@ wget https://raw.githubusercontent.com/ceph/ceph-container/master/examples/kubernetes/rbd-provisioner/@
