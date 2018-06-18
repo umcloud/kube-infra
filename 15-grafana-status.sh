2e118d2fda0002653293dafe596a00ddc7b7ee56
JuanJo Ciarlante <juanjosec@gmail.com>
2018-06-18 20:05:39 +0000

    [jjo] updates from lecture slides/examples

--- 15-grafana-status.sh
+++ 15-grafana-status.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # From: helm status grafana
-set -x
 POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana" -o jsonpath="{.items[0].metadata.name}")
-echo "Visit: http://localhost:3000/"
-echo "Run: kubectl --namespace monitoring port-forward $POD_NAME 3000"
+echo "# 1) Run: "
+echo "kubectl --namespace monitoring port-forward $POD_NAME 3000"
 pass=$(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 -d)
-echo "       user: admin  password: $pass"
-
+echo "# 2) Visit: http://localhost:3000/ with ->  user: admin  password: $pass"
 # Add Datasource:
 #   Settings:
 #     Name: prometheus
@@ -15,4 +13,3 @@ echo "       user: admin  password: $pass"
 #   URL: http://prometheus-server/
 # Add Dashboard:
 #   Import: 3119
-
